eb9e31ceee5a773011da7034283b0a5a4b0618fa
Wouter Vroege <wouter@woutervroege.nl>
2019-10-10 02:01:39 +0200

    added demo commands

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/*
+Init
+*/
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -5,18 +8,30 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 const PostController = require('./controllers/PostController');
 
+/*
+Get All Posts
+$ curl http://localhost:3000/posts
+*/
 app.get('/posts', async (req, res) => {
   const controller = new PostController();
   const postView = await controller.getPostsList();
   res.json(postView);
 })
 
+/*
+Get Post By Id
+$ curl http://localhost:3000/posts/3
+*/
 app.get('/posts/:id', async (req, res) => {
   const controller = new PostController();
   const postView = await controller.getPost(req.params.id);
   res.json(postView);
 })
 
+/*
+Create Post
+$ curl -X "POST" -H "Content-Type: application/json" -d '{"title": "Another Blog Post", "uid": "1", "timePublished": 0, "contents": "Cras mattis consectetur purus sit amet fermentum."}' http://localhost:3000/posts
+*/
 app.post('/posts', async (req, res) => {
   const controller = new PostController();
   const post = await controller.createPost(req.body);
@@ -24,6 +39,10 @@ app.post('/posts', async (req, res) => {
   res.status(status).json(post.toJSON());
 })
 
+/*
+Update Post
+$ curl -X "PUT" -H "Content-Type: application/json" -d '{"title": "Another Blog Post", "uid": "1", "timePublished": 0, "contents": "Cras mattis consectetur purus sit amet fermentum."}' http://localhost:3000/posts/3
+*/
 app.put('/posts/:id', async (req, res) => {
   const controller = new PostController();
   const post = await controller.updatePost({...req.body, id: req.params.id});
@@ -31,6 +50,10 @@ app.put('/posts/:id', async (req, res) => {
   res.status(status).json(post.toJSON());
 })
 
+/*
+Patch Post; only update provided properties
+$ curl -X "PATCH" -H "Content-Type: application/json" -d '{"title": "Just update the title"}' http://localhost:3000/posts/3
+*/
 app.patch('/posts/:id', async (req, res) => {
   const controller = new PostController();
   const post = await controller.patchPost({...req.body, id: req.params.id});
@@ -38,6 +61,10 @@ app.patch('/posts/:id', async (req, res) => {
   res.status(status).json(post.toJSON());
 })
 
+/*
+Delete Post
+$ curl -X "DELETE" http://localhost:3000/posts/3
+*/
 app.delete('/posts/:id', async (req, res) => {
   const controller = new PostController();
   const success = await controller.deletePost(req.params.id);
@@ -45,10 +72,17 @@ app.delete('/posts/:id', async (req, res) => {
   res.status(status).end();
 })
 
+/*
+Get All Posts By User Id
+$ curl http://localhost:3000/users/1/posts
+*/
 app.get('/users/:id/posts', async (req, res) => {
   const controller = new PostController();
   const postView = await controller.getUserPostsList(req.params.id);
   res.json(postView);
 })
 
+/*
+Off we go
+*/
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
